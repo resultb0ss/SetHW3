cd46fac9fc2a52812363770536768e08de493e2c
Admin <reteamboss@gmail.com>
2024-09-23 17:16:48 +0300

    3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,14 @@
+import java.util.TreeSet
+
 fun main() {
-    println("Hello World!")
+    var mySet = TreeSet<Int>()
+    mySet.addAll(arrayOf(1,2,3,4,5,6,7,8,9,10,15,20,13,14,34))
+
+    println(removeDuplicates(mySet))
+
+
+}
+
+fun <T> removeDuplicates(collection: Collection<T>?): Set<T>? {
+    return collection?.toSet()
 }
